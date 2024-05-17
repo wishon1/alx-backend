7369075eed1310d79a37289336fdd601512cf665
Wisdom Honest <117495032+wishon1@users.noreply.github.com>
2024-05-17 09:39:47 -0400

    Update 1-simple_pagination.py
    update error of pycodestyle in this file

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,12 +1,12 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 """
-Simple paginationo:
+Simple pagination:
 
 Implement a method named get_page that takes two integer arguments page with
 default value 1 and page_size with default value 10.
 
 You have to use this CSV file (same as the one presented at the top of the
-project) Use assert to verify that both arguments are integers greater than 0.
+project). Use assert to verify that both arguments are integers greater than 0.
 
 Use index_range to find the correct indexes to paginate the dataset correctly
 and return the appropriate page of the dataset (i.e. the correct list of rows).
@@ -19,18 +19,18 @@ import math
 from typing import List, Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
-    function named index_range that takes two integer arguments page and
-    page_size
+    Function named index_range that takes two integer arguments page and
+    page_size.
 
     Args:
-        page(int): the current page
-        page_size(int): the size of the page
+        page (int): the current page
+        page_size (int): the size of the page
 
-    Returns(tuple): tuple of size two containing a start index and an end
+    Returns (tuple): A tuple of size two containing a start index and an end
         index corresponding to the range of indexes to return in a list for
-        those articular pagination parameters
+        those particular pagination parameters.
     """
     start_index = (page - 1) * page_size
     end_index = page * page_size
@@ -59,23 +59,24 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
-        get_page: Retieves the page of a given data-set
+        get_page: Retrieves the page of a given dataset.
 
         Args:
-            page(int): The page to work with
-            page_size(int): The size of the page
+            page (int): The page to work with.
+            page_size (int): The size of the page.
 
-        Return: return the appropriate page of the dataset (i.e. the correct
-                list of rows).
-                else return emty list if the start index gt len(dataset())
+        Returns:
+            list: The appropriate page of the dataset (i.e., the correct list
+            of rows), or an empty list if the start index is greater than the
+            length of the dataset.
         """
         # Use assert to verify that both arguments are integers greater than 0.
-        assert isinstance(page, int) and page > 0,
-        assert isinstance(page_size), int) and page_size > 0,
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-        data_set = self.dataset()
+        dataset = self.dataset()
         start_index, end_index = index_range(page, page_size)
 
-        if start_index > len(data_set):
+        if start_index >= len(dataset):
             return []
-        return data_set[start_index:end_index]
+        return dataset[start_index:end_index]
