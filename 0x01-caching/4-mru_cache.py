26496801185b2aeede7d48323fd78ab07c840fa7
wishon1 <wisdomhonest1@gmail.com>
2024-05-22 09:19:04 -0400

    add updates to task 3 and 4

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Create a class LRUCache that inherits from BaseCaching and is a caching system:
+Create a class MRUCache that inherits from BaseCaching and is a caching system:
 
 You must use self.cache_data - dictionary from the parent class BaseCaching
 You can overload def __init__(self): but don’t forget to call the parent
@@ -9,45 +9,44 @@ def put(self, key, item):
 Must assign to the dictionary self.cache_data the item value for the key key.
 If key or item is None, this method should not do anything.
 If the number of items in self.cache_data is higher that BaseCaching.MAX_ITEMS:
-you must discard the least recently used item (LRU algorithm)
+you must discard the most recently used item (MRU algorithm)
 you must print DISCARD: with the key discarded and following by a new line
 def get(self, key):
 Must return the value in self.cache_data linked to key.
 If key is None or if the key doesn’t exist in self.cache_data, return None.
 """
-
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 
-class LRUCache(BaseCaching):
-    """LRUCache that inherits from BaseCaching and is a caching system"""
+class MRUCache(BaseCaching):
+    """
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """Initialize the class"""
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Input a new item into the cache"""
-        if key is None and item is None:
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
             pass
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                # Remove the least recently used item if the cache is full
-                first_key, _ = self.cache_data.popitem(True)
-                print(f"DISCARD: {first_key}")
-
-            # Add the new key-value pair to the cache
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
         else:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value in self.cache_data linked to key"""
-        if key is None or key not in self.cache_data:
-            return None
-
-        # Move the accessed key to the front to mark it as recently used
-        self.cache_data.move_to_end(key, last=False)
-        return self.cache_data[key]
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
