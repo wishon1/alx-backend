ba4237973de7398f398f384eac1a66600cfab792
wishon1 <wisdomhonest1@gmail.com>
2024-05-22 07:09:44 -0400

    made changes to 0-basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -14,14 +14,9 @@ If key is None or if the key doesn’t exist in self.cache_data, return None.
 """
 BaseCaching = __import__("base_caching").BaseCaching
 
+
 class BasicCache(BaseCaching):
     """BasicCache that inherits from BaseCaching and is a caching system"""
-    def __init__(self):
-        """ Initiliaze
-        """
-        # call the parent class initializer
-        super().__init__()
-
     def put(self, key, item):
         """Add an item in the cache """
         if key is not None and item is not None:
