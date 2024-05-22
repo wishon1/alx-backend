9392c61ab7d64db0536753dd1143bcfdb9b2ca47
wishon1 <wisdomhonest1@gmail.com>
2024-05-22 09:02:47 -0400

    add task 3

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -34,12 +34,12 @@ class LRUCache(BaseCaching):
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
                 # Remove the least recently used item if the cache is full
-                first_key, _ = self.cache_data.popitem(last=False)
+                first_key, _ = self.cache_data.popitem(True)
                 print(f"DISCARD: {first_key}")
 
             # Add the new key-value pair to the cache
             self.cache_data[key] = item
-            self.cache_data.move_to_end(key)
+            self.cache_data.move_to_end(key, last=False)
         else:
             self.cache_data[key] = item
 
