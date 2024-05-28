b6e3dd92ea6353eeda324348a77a405a8a7cef29
wishon1 <wisdomhonest1@gmail.com>
2024-05-28 13:34:48 -0400

    add corrections to task 0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
+def index() -> str:
     """main route"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __main__ == "__main__":
