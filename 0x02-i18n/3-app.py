5d0ce44c9ee21975a12a2c2eed691f536ae565ba
wishon1 <wisdomhonest1@gmail.com>
2024-05-29 10:52:32 -0400

    add update to task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,11 +26,22 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
-@babel.localeselector
+# @babel.localeselector( am commeting it because of this error):
+# ack (most recent call last):
+#  File "/usr/lib/python3.8/runpy.py", line 194, in _run_module_as_main
+#    return _run_code(code, main_globals, None,
+#  File "/usr/lib/python3.8/runpy.py", line 87, in _run_code
+#    exec(code, run_globals)
+#  File "/home/pc/alx-backend/0x02-i18n/3-app.py", line 29, in <module>
+#    @babel.localeselector
+#AttributeError: 'Babel' object has no attribute 'localeselector'
+ 
 def get_locale() -> str:
     """Determine the best match for the supported language"""
     return request.accept_language.best_match(app.config['LANGUAGES'])
 
+# @babel.localeselector is not supported in newer versions of babel. use this instead
+babel.init_app(app, locale_selector=locale)
 
 @app.route('/')
 def index() -> str:
