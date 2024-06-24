0ace2f1f59f8b07fa0f5be5c687755c3e6547aac
wishon1 <wisdomhonest1@gmail.com>
2024-06-24 09:40:14 -0400

    add task 1

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,17 @@
+/**
+ *  should connect to the Redis server running on your machine:
+ * It should log to the console the message Redis client connected to the server
+ * when the connection to Redis works correctly It should log to the console the
+ * message Redis client not connected to the server: ERROR_MESSAGE when the
+ * connection to Redis does not work
+ */
+import  { createClient } from 'redis'
+const clientInstance = createClient();
+
+clientInstance.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+clientInstance.on('error', (err) => {
+    console.error('Redis client not connected to the server:', err.message)
+});
