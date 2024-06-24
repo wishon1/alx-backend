c5771ac66a9fa1daec5112df3157781546250b1d
wishon1 <wisdomhonest1@gmail.com>
2024-06-24 10:43:00 -0400

    add task 2

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,44 @@
+/**
+ *  should connect to the Redis server running on your machine:
+ * It should log to the console the message Redis client connected to the server
+ * when the connection to Redis works correctly. It should log to the console the
+ * message Redis client not connected to the server: ERROR_MESSAGE when the
+ * connection to Redis does not work.
+ */
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const clientInstance = createClient();
+
+clientInstance.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+clientInstance.on('error', (err) => {
+  console.error('Redis client not connected to the server:', err.message);
+});
+
+/**
+ * Sets a new school in Redis.
+ * @param {string} schoolName - The name of the school.
+ * @param {string} value - The value to set for the school.
+ */
+function setNewSchool(schoolName, value) {
+  clientInstance.set(schoolName, value, print);
+}
+
+/**
+ * Displays the value of a school from Redis.
+ * @param {string} schoolName - The name of the school to display.
+ */
+async function displaySchoolValue(schoolName) {
+  // promisify the get method
+  const getAsync = promisify(clientInstance.get).bind(clientInstance);
+  const value = await getAsync(schoolName);
+  console.log(value);
+}
+
+// Call the functions as specified
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
